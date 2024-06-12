490f8ebee9db72734e9aa4181f85122fe06526e7
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2024-06-12 13:23:47 +0700

    feat(app): model loaded + predict done + middleware auth done + fix some configuration - firestore history - swagger doc

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ import io
 version_api = 'v1'
 
 app = Flask(__name__)
+model = None
+CLASS_NAME = ['fresh_apples','fresh_banana','fresh_orange','fresh_strawberry','rotten_apples','rotten_banana','rotten_orange','rotten_strawberry']
 
 SWAGGER_URL = '/api/docs'  
 API_URL = "/swagger/v1.json"   
@@ -50,7 +52,7 @@ def swagger_spec():
 def register():
     data = request.get_json()
     user = get_data("users", data['username'])
-    if user.exists:
+    if user:
         return jsonify({"message": "User already exists"}), 400
     add_data("users", data['username'], data)
     return jsonify({"message": "User registered successfully"}), 201
@@ -59,7 +61,7 @@ def register():
 def login():
     data = request.get_json()
     user = get_data("users", data['username'])
-    if not user.exists or user.to_dict()['password'] != data['password']:
+    if not user or user['password'] != data['password']:
         return jsonify({"message": "Invalid username or password"}), 401
     token = jwt.encode({'username': data['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, os.getenv('SECRET_KEY'), algorithm="HS256")
     return jsonify({'token': token}), 200
@@ -68,12 +70,17 @@ def login():
 def verify_token():
     token = request.headers.get('Authorization').split()[1]
     try:
-        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=["HS256"])
         return jsonify({'message': 'Token is valid', 'data': data}), 200
     except jwt.ExpiredSignatureError:
         return jsonify({'message': 'Token has expired'}), 401
     except jwt.InvalidTokenError:
         return jsonify({'message': 'Invalid token'}), 401
+    
+@app.route('/history', methods=['GET'])
+@require_authentication
+def get_history(current_user):
+    return get_data('history', current_user['username'])
 
 ############### PREDICT ROUTE AND CONFIGURATION #################
 # UPLOAD_FOLDER = 'uploads'
@@ -84,14 +91,14 @@ def verify_token():
 
 def preprocess_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    image = image.resize((224, 224))
+    image = image.resize((150, 150))
     image = np.array(image) / 255.0  # Normalize to [0, 1]
     image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
 
 @app.route('/predict', methods=['POST'])
-# @require_authentication
-def predict_image():
+@require_authentication
+def predict_image(current_user):
     try :
         # load model from file (local) -> done
         # get image request
@@ -109,11 +116,16 @@ def predict_image():
 
         # predict image with model already loaded
         prediction = model.predict(image_tensor)
-        print(prediction)
+        predicted_index = np.argmax(prediction, axis=1)[0]
+        class_name = CLASS_NAME[predicted_index]
+
+        # save to firestore as history 
+        add_data('history', f"{current_user['username']}", { 'predicted': class_name, 'timestamp': datetime.datetime.utcnow()})
 
         return jsonify({ 'message': 'Image has already been predicted', 'statusCode': 200 }), 200
-    except:
+    except Exception as err:
         # error
+        print(err)
         raise APIException('Could not predict image', 401)
 
 
@@ -127,9 +139,12 @@ def load_model():
     except Exception as error:
         print('Failed to load model:', error)
         raise error
-    
+
 ############### PREDICT ROUTE AND CONFIGURATION #################
 
-if __name__ == "__main__":
-    model = load_model()
-    app.run(debug=True)
+if __name__ == 'app':
+    try:
+        model = load_model()
+        app.run(debug=True)
+    except Exception as e:
+        print(f"Application failed to start: {e}")
